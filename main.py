269ba38a8f670984b91d2dc1e2a12c02d10cbb67
José Ricardo <kingalvl80@gmail.com>
2025-04-30 17:26:45 -0300

    tentei fazer o add de tarefas

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
+import json
 # Menu de Seleção
 
 
 
-
 # 1. Adicionar Tarefas
+try:
+    with open("tarefas.json", "r") as f:
+        lista_tarefa = json.load(f)
+except FileNotFoundError:
+    lista_tarefa = []
 
 
+while True:
+    nova_tarefa = input('Digite sua tarefa ou "sair" para terminar: ')
 
+    if nova_tarefa.lower() == 'sair':
+        break
 
-# 2. Listar Tarefas
+    lista_tarefa.append(nova_tarefa)
 
+    with open("tarefas.json", "w") as f:
+        json.dump(lista_tarefa, f, indent=4)
 
+    print("\nTarefas atualizadas:")
+    for tarefa in lista_tarefa:
+        print(f"- {tarefa}")
 
+print("\nFim da edição. Tarefas salvas com sucesso.")
 
 # 3. Marcar Tarefas
 
