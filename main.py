8650095ca30ece8d34b1b102707ed70db5fa6ffa
José Ricardo <kingalvl80@gmail.com>
2025-06-03 07:26:04 -0300

    correção de erratas e tentativa de implementar a função "Remover Tarefas"

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 #1. Adiconar Tarefa
 def adicionar_tarefas():
     '''Abre o arquivo da lista de tarefas e escreve as atividades do usario em um documento txt'''
-    with open('list_terefas.txt', 'a') as listat:
+    with open('list_tarefas.txt', 'a') as listat:
         tarefa = input('Digite sua Tarefa:\n')
-        listat.write = (tarefa + '/n')
-
-    '''Abre o arquivo no modo de leitura para que o usuario veja se o que o mesmo escreveu foi salvo corretamente'''
-    with open('list_terefas.txt', 'r') as listat:
-        conteudo = listat.read()
-        print('/nSuas tarefas:')
-        print(conteudo)
+        listat.write    (tarefa + '\n')
 
 #----------------------------------------------------------------------------------------------------------------------
 #2. Ver Tarefas
 def todas_tarefas():
     '''Abre o arquivo com as tarefas e mostra ao usuario'''
-    with open('list_terefas.txt', 'r') as listat:
+    with open('list_tarefas.txt', 'r') as listat:
         total = listat.read()
-        print('/nEssas são suas tarefas:')
+        print('\nEssas são suas tarefas:')
         print(total)
         print('Já finalizou alguma hoje? Vamos dar o nosso melhor! 😄')
 
+#----------------------------------------------------------------------------------------------------------------------
+#4. Remover Tarefas
+def remover_tarefas():
+    arquivo = "list_tarefas.txt"
+    '''Seleciona o texto a ser deletado'''
+    print("Digite parte do conteúdo da linha que deseja excluir:")
+    texto = input()
+    '''Abre o arquivo e lê todas as linhas'''
+    abrido = open(arquivo, "r")
+    linhas = abrido.readlines()
+    abrido.close()
+    '''Mostra as linhas que contêm o texto'''
+    for i in range(len(linhas)):
+        if texto in linhas[i]:
+            print(i, ":", linhas[i].strip())
+    '''Pede o número da linha a remover'''
+    print("Digite o número da linha que deseja remover:")
+    num = int(input())
+    '''Remove a linha escolhida'''
+    del linhas[num]
+    '''Reescreve o arquivo'''
+    abrido = open(arquivo, "w")
+    abrido.writelines(linhas)
+    abrido.close()
+
+    print("Linha removida com sucesso.")
+
+menu()
